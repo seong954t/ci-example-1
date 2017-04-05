24fd4edb4ae6e6dae2f20222f7d0a75c81ba9383
SeoungTae <seong954t@gmail.com>
2017-04-05 22:11:37 +0900

    CalculatorTest.java

--- src/test/java/univ/lecture/CalculatorTest.java
+++ src/test/java/univ/lecture/CalculatorTest.java
@@ -17,6 +17,9 @@ public class CalculatorTest {
 		assertThat(output, is(2));
 		output = calc.calculate("3+2*(5-2)+3*2");
 		assertThat(output, is(15));
+		output = calc.calculate("3+(5-1)*4");
+		assertThat(output, is(19));
+
 	}
 
 	@Test
@@ -26,6 +29,8 @@ public class CalculatorTest {
 		assertThat(output, is(-1));
 		output = calc.calculate("3+2*((5-2)+3)*2");
 		assertThat(output, is(27));
+		output = calc.calculate("7-(6+3)/2+1");
+		assertThat(output, is(4));
 	}
 
 	@Test
@@ -35,6 +40,8 @@ public class CalculatorTest {
 		assertThat(output, is(30));
 		output = calc.calculate("(3+2)*((5-2)+3)*2");
 		assertThat(output, is(60));
+		output = calc.calculate("8+1*2*(3+(7-1))");
+		assertThat(output, is(26));
 	}
 
 	@Test
@@ -44,5 +51,8 @@ public class CalculatorTest {
 		assertThat(output, is(3));
 		output = calc.calculate("(5+3)*2");
 		assertThat(output, is(16));
+		output = calc.calculate("(4-2)+8/2");
+		assertThat(output, is(6));
+
 	}
 }
