b6cd3d3aed4ef21f6f5fd31e8c4081f3ddc608f4
eunjeong Jang <cs801458j@gmail.com>
2017-04-05 18:09:21 +0900

    ej commit

--- src/main/java/univ/lecture/Calculator.java
+++ src/main/java/univ/lecture/Calculator.java
@@ -1,10 +1,94 @@
 package univ.lecture;
 
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
 /**
  * Created by tchi on 2017. 3. 19..
  */
 public class Calculator {
-    public int calculate(String exp) {
-        return 2;
-    }
+
+	public int calculate(String exp) {
+	      int data = 0;
+	      if (exp.contains(")")) {
+	         int q = 0;
+	         Stack stack = new Stack<>();
+	         while (exp.length() != q) {
+	            String calcul = "";
+	            char cal;
+	            while (exp.length() != q) {
+	               if((cal = exp.charAt(q++)) == ')'){
+	                  break;
+	               }
+	               stack.push(cal);
+	            }
+	            while (!stack.empty()) {
+	               if((char) stack.peek() == '('){
+	                  stack.pop();
+	                  break;
+	               }
+	               calcul = (char) stack.pop() + calcul;
+	            }
+	            String v = Integer.toString(calculate(calcul));
+	            int i = 0;
+	            while(v.length() != i){
+	               stack.push(v.charAt(i++));
+	            }
+	         }
+	         exp = "";
+	         while (!stack.empty()) {
+	            exp = stack.pop()+exp;
+	         }
+	      }
+	      if (exp.contains("+")) {
+	         StringTokenizer st = new StringTokenizer(exp, "+");
+	         int cal = calculate(st.nextToken());
+	         while (st.hasMoreTokens()) {
+	            String name = st.nextToken();
+	            cal += calculate(name);
+	         }
+	         data = cal;
+	      } else if (exp.contains("-")) {
+	         StringTokenizer st = new StringTokenizer(exp, "-");
+	         String val = st.nextToken();
+	         char set = val.charAt(val.length() - 1);
+	         int cal;
+	         if (set == '*' || set == '/') {
+	            cal = calculate(st.nextToken());
+	            val += cal;
+	            cal = -calculate(val);
+	         } else {
+	            cal = calculate(val);
+	            if (!st.hasMoreTokens()) {
+	               cal = -cal;
+	            }
+	         }
+
+	         while (st.hasMoreTokens()) {
+	            String name = st.nextToken();
+	            cal -= calculate(name);
+	         }
+	         data = cal;
+	      } else if (exp.contains("*")) {
+	         StringTokenizer st = new StringTokenizer(exp, "*");
+	         int cal = calculate(st.nextToken());
+	         while (st.hasMoreTokens()) {
+	            String name = st.nextToken();
+	            cal *= calculate(name);
+	         }
+	         data = cal;
+	      } else if (exp.contains("/")) {
+	         StringTokenizer st = new StringTokenizer(exp, "/");
+	         int cal = calculate(st.nextToken());
+	         while (st.hasMoreTokens()) {
+	            String name = st.nextToken();
+	            cal /= calculate(name);
+	         }
+	         data = cal;
+	      } else {
+	         data = Integer.parseInt(exp);
+	      }
+	      return data;
+	   }
 }
