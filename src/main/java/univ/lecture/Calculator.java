b14350c75194cff7fa9446a9343d2c42bc250e45
Kim Daeun <kde0820@gmail.com>
2017-04-05 18:08:14 +0900

    Restore Code

--- src/main/java/univ/lecture/Calculator.java
+++ src/main/java/univ/lecture/Calculator.java
@@ -1,6 +1,5 @@
 package univ.lecture;
 
-import java.util.ArrayList;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -10,108 +9,85 @@ import java.util.StringTokenizer;
 public class Calculator {
 
 	public int calculate(String exp) {
-		StringTokenizer st = new StringTokenizer(exp, "+-*/()", true);
-		ArrayList<String> exps = new ArrayList<>();
-		
-		while (st.hasMoreTokens()) {
-			String token = st.nextToken();
-			exps.add(token);
-		}
-		
-		
-		return calRPN(infixToPostfix(exps.toArray(new String[exps.size()])));
-	}
+	      int data = 0;
+	      if (exp.contains(")")) {
+	         int q = 0;
+	         Stack stack = new Stack<>();
+	         while (exp.length() != q) {
+	            String calcul = "";
+	            char cal;
+	            while (exp.length() != q) {
+	               if((cal = exp.charAt(q++)) == ')'){
+	                  break;
+	               }
+	               stack.push(cal);
+	            }
+	            while (!stack.empty()) {
+	               if((char) stack.peek() == '('){
+	                  stack.pop();
+	                  break;
+	               }
+	               calcul = (char) stack.pop() + calcul;
+	            }
+	            String v = Integer.toString(calculate(calcul));
+	            int i = 0;
+	            while(v.length() != i){
+	               stack.push(v.charAt(i++));
+	            }
+	         }
+	         exp = "";
+	         while (!stack.empty()) {
+	            exp = stack.pop()+exp;
+	         }
+	      }
+	      if (exp.contains("+")) {
+	         StringTokenizer st = new StringTokenizer(exp, "+");
+	         int cal = calculate(st.nextToken());
+	         while (st.hasMoreTokens()) {
+	            String name = st.nextToken();
+	            cal += calculate(name);
+	         }
+	         data = cal;
+	      } else if (exp.contains("-")) {
+	         StringTokenizer st = new StringTokenizer(exp, "-");
+	         String val = st.nextToken();
+	         char set = val.charAt(val.length() - 1);
+	         int cal;
+	         if (set == '*' || set == '/') {
+	            cal = calculate(st.nextToken());
+	            val += cal;
+	            cal = -calculate(val);
+	         } else {
+	            cal = calculate(val);
+	            if (!st.hasMoreTokens()) {
+	               cal = -cal;
+	            }
+	         }
 
-	private String[] infixToPostfix(String[] exp) {
-		Stack<String> stack = new Stack<>();
-		ArrayList<String> postfix = new ArrayList<>();
-		
-		for (int i = 0; i < exp.length; i++) {
-			if (isAnOperator(exp[i])) {
-				if (stack.isEmpty()) {
-					stack.push(exp[i]);
-				} else {
-					while (true) {
-						if (stack.isEmpty()) {
-							stack.push(exp[i]);
-							break;
-						} else {
-							if (precedence(stack.peek()) < precedence(exp[i])) {
-								stack.push(exp[i]);
-								break;
-							} else
- 								postfix.add(stack.pop());
-						}
-
-					}
-				}
-			} else if (exp[i].equals("(")) {
-				stack.push(exp[i]);
-			} else if (exp[i].equals(")")) {
-				while (!stack.peek().equals("(")) {
-					postfix.add(stack.pop().toString());
-				}
-				if (stack.peek().equals("(")) {
-					stack.pop();
-				}
-			}
-			/* 피연산자일 때 */
-			else {
-				postfix.add(exp[i]); // 후위식 배열에 넣어줌
-			}
-			/* eos일 때 */
-			if (i == exp.length - 1) {
-				while (!stack.isEmpty()) {
-					postfix.add(stack.pop());
-				}
-			}
-		}
-
-		return postfix.toArray(new String[postfix.size()]);
-	}
-
-	private int calRPN(String[] postfix) {
-		Stack<String> cal = new Stack<>();
-
-		for (int i = 0; i < postfix.length; i++) {
-			String input = postfix[i];
-
-
-			if (isAnOperator(input)) {
-				int y = Integer.parseInt(cal.pop());
-				int x = Integer.parseInt(cal.pop());
-				int z = evaluate(x, y, input);
-				cal.push(z+"");
-			} else
-				cal.push(input);
-		}
-		return Integer.parseInt(cal.peek());
-	}
-
-	private int evaluate(int x, int y, String op) {
-		int z;
-
-		if (op.equals("+"))
-			z = x + y;
-		else if (op.equals("-"))
-			z = x - y;
-		else if (op.equals("*"))
-			z = x * y;
-		else
-			z = x / y;
-		return z;
-	}
-
-	private int precedence(String token) {
-		if (token.equals("+") || token.equals("-"))
-			return 2;
-		else if (token.equals("*") || token.equals("/"))
-			return 4;
-		else
-			return 0;
-	}
-
-	private boolean isAnOperator(String s) {
-		return "+-/*".indexOf(s) >= 0;
-	}
+	         while (st.hasMoreTokens()) {
+	            String name = st.nextToken();
+	            cal -= calculate(name);
+	         }
+	         data = cal;
+	      } else if (exp.contains("*")) {
+	         StringTokenizer st = new StringTokenizer(exp, "*");
+	         int cal = calculate(st.nextToken());
+	         while (st.hasMoreTokens()) {
+	            String name = st.nextToken();
+	            cal *= calculate(name);
+	         }
+	         data = cal;
+	      } else if (exp.contains("/")) {
+	         StringTokenizer st = new StringTokenizer(exp, "/");
+	         int cal = calculate(st.nextToken());
+	         while (st.hasMoreTokens()) {
+	            String name = st.nextToken();
+	            cal /= calculate(name);
+	         }
+	         data = cal;
+	      } else {
+	         data = Integer.parseInt(exp);
+	      }
+	      return data;
+	   }
 }
