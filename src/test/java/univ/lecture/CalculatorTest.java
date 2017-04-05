5a5e5d48266cca3f8a49455076e80d48bce297c3
SeoungTae <seong954t@gmail.com>
2017-04-05 14:17:03 +0900

    Modify Calculator.java

--- src/test/java/univ/lecture/CalculatorTest.java
+++ src/test/java/univ/lecture/CalculatorTest.java
@@ -14,5 +14,23 @@ public class CalculatorTest {
         Calculator calc = new Calculator();
         int output = calc.calculate("1+1");
         assertThat(output, is(2));
+        output = calc.calculate("1+2");
+        assertThat(output, is(3));
+        output = calc.calculate("3+2*(5-2)+3*2");
+        assertThat(output, is(15));
+        output = calc.calculate("3+2*((5-2)+3)*2");
+        assertThat(output, is(27));
+        output = calc.calculate("(3+2)*((5-2)+3)*2");
+        assertThat(output, is(60));
+        output = calc.calculate("3-4");
+        assertThat(output, is(-1));
+        output = calc.calculate("5*6");
+        assertThat(output, is(30));
+        output = calc.calculate("(5+3)*2");
+        assertThat(output, is(16));
+        output = calc.calculate("(5+3)/2");
+        assertThat(output, is(4));
+        output = calc.calculate("1+2");
+        assertThat(output, is(3));
     }
 }
